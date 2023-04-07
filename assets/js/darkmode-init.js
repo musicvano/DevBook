32c053e84a85a1d45a3b985e110a7d5ee9a8e68e
Ivan Muzyka <musicvano@gmail.com>
2023-04-07 21:24:14 +0300

    Update darkmode-init.js

--- assets/js/darkmode-init.js
+++ assets/js/darkmode-init.js
@@ -2,20 +2,14 @@ const globalDark = window.matchMedia && window.matchMedia('(prefers-color-scheme
 const localMode = localStorage.getItem('theme');
 
 if (globalDark && (localMode === null)) {
-
   localStorage.setItem('theme', 'dark');
   document.documentElement.setAttribute('data-dark-mode', '');
-
 }
 
 if (globalDark && (localMode === 'dark')) {
-
   document.documentElement.setAttribute('data-dark-mode', '');
-
 }
 
 if (localMode === 'dark') {
-
   document.documentElement.setAttribute('data-dark-mode', '');
-
 }
